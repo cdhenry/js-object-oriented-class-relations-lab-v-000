6d1c4f8d2d9a3e56d304063ef88b3509734bde1f
Chris Henry <cdhenry@gmail.com>
2018-04-23 15:17:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,17 +32,17 @@ class Passenger{
     store.passengers.push(this);
   }
 
+  trips(){
+    return store.trips.filter(trip => {
+      return trip.passengerId === this.id;
+    })
+  }
+
   drivers(){
     return store.drivers.filter(driver => {
       return driver.trips().filter(trip => {
         return trip.passengerId === this.id;
       })
-    })
-  }
-
-  trips(){
-    return store.trips.filter(trip => {
-      return trip.passengerId === this.id;
     })
   }
 }
